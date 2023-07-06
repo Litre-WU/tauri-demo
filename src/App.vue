e82cd5c944cd9c5c382a3f0e99ce6851e8ad189f
litre-wu <litre-wu@tutanota.com>
2023-07-06 14:25:47 +0800

    第二次提交

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Greet from "./components/Greet.vue";
+import Demo from "./components/Demo.vue"
 </script>
 
 <template>
-  <div class="container">
-    <Greet />
-  </div>
+<!--  <div class="container">-->
+<!--    <Greet/>-->
+<!--  </div>-->
+  <Demo/>
 </template>
 
 <style scoped>
---
+++ src/components/Demo.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="content"><img src="https://www.litre.tk/image/header/home.jpg" alt="测试"><div><p>测试文本</p></div></div>
+  <div class="content"><img src="https://www.litre.tk/image/header/home.jpg" alt="测试"><div><p>测试文本</p></div></div>
+</template>
+
+<style scoped>
+
+</style>
