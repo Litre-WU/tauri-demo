8f782d87b1aaa1fb32ba3cd4937c690368fd36dd
litre-wu <litre-wu@tutanota.com>
2023-07-22 15:30:29 +0800

    第三次提交

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,34 +1,5 @@
 <script setup>
 
-const container = document.getElementById('app');
-
-// 初始化数据
-async function init() {
-  fetch('http://10.2.106.77:8000/test', {
-    'method': 'POST',
-    body: JSON.stringify({test: 1}),
-    headers: {'Content-Type': 'application/json'}
-  })
-      .then(response => response.json())
-      .then(data => {
-        // const container = document.getElementById('app');
-        data.forEach(item => {
-          const divElement = document.createElement('div');
-          const imgElement = document.createElement('img');
-          const paragraphElement = document.createElement('p');
-          imgElement.src = item.img;
-          paragraphElement.textContent = item.text;
-          divElement.setAttribute('class', 'content')
-          divElement.appendChild(imgElement)
-          divElement.appendChild(paragraphElement);
-          container.appendChild(divElement);
-        });
-      });
-}
-
-init();
-
-
 // 下拉追加数据
 async function fetchData(page) {
   fetch('http://10.2.106.77:8000/test', {
@@ -38,7 +9,7 @@ async function fetchData(page) {
   })
       .then(response => response.json())
       .then(data => {
-        // const container = document.getElementById('app');
+        const container = document.getElementById('app');
         data.forEach(item => {
           const divElement = document.createElement('div');
           const imgElement = document.createElement('img');
@@ -54,16 +25,17 @@ async function fetchData(page) {
 }
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  const element = document.getElementById('app').firstElementChild;
-  element.addEventListener('scroll', () => {
-    console.log(">>>");
-    if (element.scrollTop + element.clientHeight >= element.scrollHeight) {
-      console.log("????");
-      // fetchData(2);
-    }
-  });
+var page = 1;
+
+fetchData(page);
+
+window.addEventListener('scroll', () => {
+  if ((window.innerHeight + window.scrollY) / document.body.offsetHeight >= 1) {
+    page += 1;
+    fetchData(page);
+  }
 });
+
 
 </script>
 
